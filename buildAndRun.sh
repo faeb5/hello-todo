df0322d8fa4f5139a1dcd8e0e133933e617627b3
faeb5 <fabian@fabianwinkelmann.xyz>
2024-11-29 19:25:21 +0100

    update build script

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn clean package && docker build -t org.example/hello-todo:latest .
-docker stop hello-todo || true && docker run --rm -d -p 8080:8080 --name hello-todo org.example/hello-todo:latest 
+mvn clean package && docker build -t org.example/hello-todo .
+docker rm -f hello-todo || true && docker run -d -p 8080:8080 --name hello-todo org.example/hello-todo
