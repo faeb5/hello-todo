3e1ea33ebcf82ec891723d2ccc68eef3d23a3cd3
faeb5 <fabian@fabianwinkelmann.xyz>
2024-11-29 19:05:58 +0100

    move from compose to simple dockerfile

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn clean package
-docker compose -f compose.yaml down || true && docker compose -f compose.yaml up -d
+mvn clean package && docker build -t org.example/hello-todo:latest .
+docker stop hello-todo || true && docker run --rm -d -p 8080:8080 --name hello-todo org.example/hello-todo:latest 
