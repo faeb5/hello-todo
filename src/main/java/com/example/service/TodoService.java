765b72b406cb9b43abe40e6084bad30bc0b395ac
faeb5 <fabian@fabianwinkelmann.xyz>
2024-11-29 18:22:27 +0100

    add todo service

---
+++ src/main/java/com/example/service/TodoService.java
@@ -0,0 +1,32 @@
+package com.example.service;
+
+import com.example.entity.Todo;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+public class TodoService {
+
+  @PersistenceContext private EntityManager entityManager;
+
+  public Todo createTodo(Todo todo) {
+    entityManager.persist(todo);
+    return todo;
+  }
+
+  public Todo updateTodo(Todo todo) {
+    entityManager.merge(todo);
+    return todo;
+  }
+
+  public Todo findTodoById(Long id) {
+    return entityManager.find(Todo.class, id);
+  }
+
+  public List<Todo> findAllTodos() {
+    return entityManager.createQuery("SELECT t FROM Todo t", Todo.class).getResultList();
+  }
+}
